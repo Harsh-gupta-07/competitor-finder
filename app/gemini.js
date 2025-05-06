a88d4c028e0b4d07d1ac3bf5d870f4241791ac67
Harshvardhan Gupta <harshvardhangofficial@gmail.com>
2025-05-07 00:23:25 +0530

    init

--- app/gemini.js
+++ app/gemini.js
@@ -1,14 +1,18 @@
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({ apiKey:  process.env.NEXT_PUBLIC_API_KEY});
-
 export async function main(startup) {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.0-flash",
-    contents: `list startups like ${startup}, listing thier features such as focus area, key features ,strength, valuation . give the response in this format || name : all info in one paragraph || make sure to only respond with name and info`,
-  });
-  console.log(response.text.replaceAll("*",""));
+  try{
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: `list startups like ${startup}, listing thier features such as focus area, key features ,strength, valuation . give the response in this format || name : all info in one paragraph || make sure to only respond with name and info`,
+    });
+    console.log(response.text.replaceAll("*",""));
+    
+    
+    return (response.text.replaceAll("*","").replaceAll("\n",""));
+  }catch(err){
+    return `temp||${err}|| temp`
+  }
   
-  
-  return (response.text.replaceAll("*","").replaceAll("\n",""));
 }
--- app/page.js
+++ app/page.js
@@ -17,7 +17,6 @@ export default function Home() {
     setAns(temp);
     setName("");
     setLoad(false);
-    // console.log(temp);
   }
 
   return (
