b5752dd949bd542dad38f2f06f0f79162cb2bafd
Harshvardhan Gupta <harshvardhangofficial@gmail.com>
2025-05-07 01:18:18 +0530

    final

--- app/api/gemini/route.js
+++ app/api/gemini/route.js
@@ -1,17 +1,17 @@
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export async function POST(req) {
-
   const { startup } = await req.json();
-  const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
+  const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
   try {
-    const result = await ai.models.generateContent({
-      model: "gemini-2.0-flash",
-      contents: `list startups like ${startup}, listing their features such as focus area, key features, strength, valuation. give the response in this format || name : all info in one paragraph || make sure to only respond with name and info. make sure to list a couple`,
-    });
+    const result = await model.generateContent([
+      `list startups like ${startup}, listing their features such as focus area, key features, strength, valuation. give the response in this format || name : all info in one paragraph || make sure to only respond with name and info. make sure to least a couple`,
+    ]);
 
-    const cleanText = result.text.replaceAll("*", "").replaceAll("\n", "");
+    const cleanText = result.response.text().replaceAll("*", "").replaceAll("\n", "");
     return Response.json({ text: cleanText });
   } catch (err) {
     console.error("Gemini error:", err);
