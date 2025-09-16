b0f64e3f89affc1051b0b902d062703dff90db1b
Harshvardhan Gupta <harshvardhangofficial@gmail.com>
2025-09-16 22:01:29 +0530

    Update route.js

--- app/api/gemini/route.js
+++ app/api/gemini/route.js
@@ -3,7 +3,6 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 export async function POST(req) {
   const { startup } = await req.json();
   const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-  // console.log("vercel", process.env.API_KEY);
   
 
   const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
